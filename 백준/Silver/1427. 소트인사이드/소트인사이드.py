89b28ebdf0fbfd712cfc51397d1acc72288a3f8b
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-08-17 01:20:26 +0900

    [Silver V] Title: 소트인사이드, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,5 +1,9 @@
-n = list(map(int, input()))
-n.sort(reverse=True)
+arr = list(map(int, input()))
+n = len(arr)
 
-for i in n:
-    print(i,end='')
+for i in range(n-1, -1, -1):
+    for j in range(i):
+        if arr[j] < arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+for k in range(n):
+    print(arr[k],end='')
