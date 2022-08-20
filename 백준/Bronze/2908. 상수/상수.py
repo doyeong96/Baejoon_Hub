77cb98d8df3af90f571bd31ed3476f63594de7a2
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-08-20 15:49:01 +0900

    [Bronze II] Title: 상수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -5,7 +5,8 @@ b = []
 for i in range(n):
     a.append(numA[n-i-1])
     b.append(numB[n-i-1])
-a, b = int(''.join(a)), int(''.join(b))
+# a, b = int(''.join(a)), int(''.join(b))
+a, b = ''.join(a), ''.join(b)
 
 if a < b:
     print(b)
