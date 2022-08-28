425fe7c6b8e716ab9c479e4302930f368b48612e
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-08-28 19:11:40 +0900

    [Silver IV] Title: 빙고, Time: 116 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -0,0 +1,37 @@
+dr = [0, 1, 1, 1]
+dc = [1, 0, 1, -1]
+
+n = 5
+board = [list(map(int, input().split())) for _ in range(n)]
+check_board = [['0'] * n for _ in range(n)]
+# Rbingo = 0
+cnt = 0 #사회자 숫자 부르면 증가
+# d = 0
+for _ in range(n):
+    mc = list(map(int, input().split()))
+    for i in range(n):
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] == mc[i]:
+                    check_board[r][c] = 'X'
+                    cnt += 1
+
+                    Rbingo = 0
+                    for sr in range(n):
+                        for sc in range(n):
+                            if check_board[sr][sc] == 'X':
+                                for d in range(4):
+                                    bingo = 0
+                                    for j in range(5):
+                                        nr = sr + dr[d] * j
+                                        nc = sc + dc[d] * j
+                                        if 0 <= nr < n and 0 <= nc < n and check_board[nr][nc] == 'X':
+                                            bingo += 1
+                                        else:
+                                            break
+                                    if bingo == 5:
+                                        Rbingo += 1
+
+                                    if Rbingo == 3:
+                                        print(cnt)
+                                        exit()
