3dc7fd3aee35b1ff44782beb052482f85d115373
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-09-20 20:48:31 +0900

    [Silver V] Title: 숫자 카드, Time: 616 ms, Memory: 253052 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,14 @@
+n = int(input())
+nums = set(map(int, input().split()))
+m = int(input())
+T_nums = list(map(int, input().split()))
+# nums = set(nums)
+'''
+T_nums 의 숫자가
+nums에 있는지를 출력해야함
+'''
+for i in range(m):
+    if T_nums[i] in nums:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
