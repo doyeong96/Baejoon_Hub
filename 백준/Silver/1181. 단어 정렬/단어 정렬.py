ba8c05aaa3c2839e56e8f209adb650919c02fb52
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-09-20 21:16:45 +0900

    [Silver V] Title: 단어 정렬, Time: 204 ms, Memory: 120040 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -2,6 +2,6 @@ n = int(input())
 words = [input() for _ in range(n)]
 words = list(set(words))
 words = sorted(words)
-words.sort(key=lambda x: len(x))
+words = sorted(words, key=lambda x: len(x))
 for words in words:
     print(words)
