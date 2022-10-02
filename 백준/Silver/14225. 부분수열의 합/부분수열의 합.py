54f6f63f1e38d9dee64de620be4e2217ea7b21e9
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-10-02 21:29:59 +0900

    [Silver I] Title: 부분수열의 합, Time: 468 ms, Memory: 194256 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -1,22 +1,19 @@
-
-def make_sum(dep, sub):
+def make_sum(dep, cur_sum):
     # 부분집합 만들기 복습좀 하자
     if dep == len(nums):
-        sum_li.add(sum(sub))
+        sum_li.add(cur_sum)
         return
     for i in range(2):
         if i:
-            sub.append(nums[dep])
-            make_sum(dep + 1, sub)
-            sub.pop()
+            make_sum(dep + 1, cur_sum+nums[dep])
         else:
-            make_sum(dep + 1, sub)
+            make_sum(dep + 1, cur_sum)
 
 
 n = int(input())
 nums = list(map(int, input().split()))
 sum_li = set()
-make_sum(0, [])
+make_sum(0, 0)
 list(sum_li).sort()
 for i in range(len(sum_li) + 2):
     if i not in sum_li:
