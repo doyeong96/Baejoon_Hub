fc1dbff9bbbc94b3c615b4702e6b604d9f7e1fb9
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-10-03 21:06:06 +0900

    [Silver II] Title: N과 M (11), Time: 384 ms, Memory: 130016 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,17 +1,17 @@
 def bt(dep, pick):
     if len(pick) == m:
-        li.add(tuple(pick))
+        print(*pick)
         return
+    ov = 0
     for i in range(n):
-        pick.append(arr[i])
-        bt(i, pick)
-        pick.pop()
+        if ov != arr[i]:
+            ov = arr[i]
+            pick.append(arr[i])
+            bt(i, pick)
+            pick.pop()
 
 
 n, m = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
 li = set()
 bt(0, [])
-li = sorted(list(li))
-for li in li:
-    print(*li)
