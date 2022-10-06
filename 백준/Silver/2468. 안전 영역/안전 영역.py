2925c7e4741999a0fc0dc32be169e8779a1b1106
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-10-06 09:45:47 +0900

    [Silver I] Title: 안전 영역, Time: 280 ms, Memory: 118468 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+dr = [0, 0, -1, 1]
+dc = [-1, 1, 0, 0]
+
+
+def bfs(sr, sc, rain):
+    global visit
+    Q = deque()
+    Q.append((sr, sc))
+    visit[sr][sc] = 1
+
+    while Q:
+        r, c = Q.popleft()
+        for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+            if 0 <= nr < n and 0 <= nc < n:
+                # 미방문 상태 이고 강수량 보다 높은 위치
+                if visit[nr][nc] == 0 and arr[nr][nc] > rain:
+                    Q.append((nr, nc))
+                    visit[nr][nc] = 1
+
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+# minV = 987654321
+# maxV = -987654321
+# for i in range(n):
+#     for j in range(n):
+#         if maxV < arr[i][j]:
+#             maxV = arr[i][j]
+maxV = max(max(arr))
+# print(maxV)
+dap = 0
+# 잠길수있는 최소부터
+for i in range(maxV):
+    visit = [[0] * n for _ in range(n)]
+    safe = 0
+    for r in range(n):
+        for c in range(n):
+            if visit[r][c] == 0 and arr[r][c] > i:
+                bfs(r, c, i)
+                safe += 1
+
+    if dap < safe:
+        dap = safe
+
+print(dap)
