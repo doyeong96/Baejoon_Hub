d7b98098bff859ba5008011bb374947b040e9064
BANNA <67449081+doyeong96@users.noreply.github.com>
2022-11-01 09:04:18 +0900

    [Silver III] Title: 두 수의 합, Time: 144 ms, Memory: 129480 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,43 +1,18 @@
-'''
-9
-5 12 7 10 9 1 2 3 11
-13
-'''
 n = int(input())
 arr = sorted(list(map(int, input().split())))
 x = int(input())
 # print(arr)
 cnt = 0
-# while l != r:
-#     if arr[l] + arr[r] == x:
-#         cnt += 1
-#         r += 1
-#     else:
-#         r += 1
-#     if r == end:
-#         l += 1
-#         r = l + 1
-# slow = 0
-# while True:
-#     for fast in range(slow+1, n):
-#         if arr[slow] + arr[fast] == x:
-#             cnt += 1
-#     else:
-#         slow += 1
-#     if slow == n:
-#         break
-# print(cnt)
-# print(s)
-# 1667229558.1148584
-# 1667229592.6003969
-
-for i in range(n - 1):
-    for j in range(i+1, n):
-        if arr[i] + arr[j] == x:
-            cnt += 1
-        if arr[i] + arr[j] > x:
-            break
-
+l = 0
+r = n - 1
+while l < r:
+    s = arr[l] + arr[r]
+    if s == x:
+        cnt += 1
+        l += 1
+    elif s > x:
+        r -= 1
+    elif s < x:
+        l += 1
 
 print(cnt)
-# print(s)
