33bf571ae07ebced41131f6ec6085b116abbae68
BANNA <67449081+doyeong96@users.noreply.github.com>
2023-04-18 09:22:52 +0900

    [Silver I] Title: 장기자랑, Time: 136 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Silver/27277. 장기자랑/장기자랑.py
+++ 백준/Silver/27277. 장기자랑/장기자랑.py
@@ -1,18 +1,15 @@
 import sys
+
 input = sys.stdin.readline
-
-from collections import deque
-
 n = int(input())
 
-li = list(map(int, input().split()))
-li.sort()
-Q = deque(li)
+Q = list(map(int, input().split()))
+Q.sort()
 m = []
 
-end = n-1
+end = n - 1
 start = 0
-while start<=end:
+while start <= end:
     m.append(Q[end])
     m.append(Q[start])
     end -= 1
