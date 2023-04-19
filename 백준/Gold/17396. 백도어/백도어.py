59967be3e0142b076df07ac33dd8501ca6386300
BANNA <67449081+doyeong96@users.noreply.github.com>
2023-04-19 12:05:58 +0900

    [Gold V] Title: 백도어, Time: 896 ms, Memory: 156600 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.py
+++ 백준/Gold/17396. 백도어/백도어.py
@@ -1,8 +1,11 @@
 import sys
+
 input = sys.stdin.readline
 
 from heapq import heappop, heappush
+
 INF = float('inf')
+
 
 def djk(start):
     Q = []
@@ -14,11 +17,11 @@ def djk(start):
         if node > road[now]:
             continue
         # 그래프에는 길이, 다음위치 순으로 저장되어 있음
-        for nway, next in g[now]:
-            cost = nway + node
-            if road[next] > cost and w[next] == 0:
-                road[next] = cost
-                heappush(Q, (cost, next))
+        for i in g[now]:
+            cost = i[0] + node
+            if road[i[1]] > cost and w[i[1]] == 0:
+                road[i[1]] = cost
+                heappush(Q, (cost, i[1]))
 
 
 n, m = map(int, input().split())
@@ -33,7 +36,7 @@ for _ in range(m):
     g[a].append((c, b))
     g[b].append((c, a))
 djk(0)
-if road[n-1] == INF:
+if road[n - 1] == INF:
     print(-1)
 else:
-    print(road[n-1])
+    print(road[n - 1])
