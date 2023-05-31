a27064ebedd257849d9d84c27d4b1ec51cfa30fd
BANNA <67449081+doyeong96@users.noreply.github.com>
2023-05-31 09:27:58 +0900

    [Silver I] Title: 주지수, Time: 964 ms, Memory: 81400 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.py
@@ -0,0 +1,20 @@
+import sys
+
+# sys.stdin = open('input.txt')
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+psum = [[0] * (m + 1) for _ in range(n + 1)]
+
+for r in range(1, n + 1):
+    for c in range(1, m + 1):
+        psum[r][c] += arr[r - 1][c - 1] - psum[r - 1][c - 1] + psum[r][c - 1] + psum[r - 1][c]
+
+for _ in range(int(input())):
+    r1, c1, r2, c2 = map(int, input().split())
+    r1, c1, r2, c2 = r1 - 1, c1 - 1, r2 - 1, c2 - 1
+    # psum[r2 + 1][c2 + 1] + psum[r1][c1] - psum[r2 - 1][c1] - psum[r1][c2 - 1]
+    a = psum[r2 + 1][c2 + 1] - psum[r1][c2 + 1] - psum[r2 + 1][c1] + psum[r1][c1]
+    print(a)
