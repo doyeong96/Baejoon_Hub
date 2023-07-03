386e86da31c91830f86a3dcac3a9acbcc0b9f435
BANNA <67449081+doyeong96@users.noreply.github.com>
2023-07-03 21:10:42 +0900

    [Silver V] Title: 귀걸이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1380. 귀걸이/귀걸이.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/1380
+import sys
+
+# sys.stdin = open("sample.txt", 'r')
+input = sys.stdin.readline
+
+cnt = 1
+while True:
+    n = int(input())
+
+    if n == 0:
+        exit()
+
+    name = [0] * (n + 1)
+    for i in range(n):
+        name[i + 1] = input().strip()
+
+    call = {}
+    for _ in range(2 * n - 1):
+        a, b = input().split()
+        a = int(a)
+        if a in call:
+            call[a] += 1
+        else:
+            call[a] = 1
+
+    for x, y in call.items():
+        if y % 2 == 1:  # 못찾음
+            print(cnt, name[x])
+    cnt += 1
